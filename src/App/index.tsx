5ef3dc585a98e7244b55780725afe9d00a0bc348
gabin.bloquet <gabin.bloquet@decathlon.com>
2020-12-19 18:56:15 +0100

    feat: add header :art:

---
+++ src/App/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.scss';
+
+function App() {
+    return (
+        <div className="App">
+            <header>
+                <img  alt='TKL Academy' src='https://www.tklacademy.com/assets/images/logo.png'/>
+                <h1>TKL DMA</h1>
+            </header>
+            <h2>Bienvenue dans l'outil qui va t'aider à constuire ta liberté financière</h2>
+        </div>
+    );
+}
+
+export default App;
